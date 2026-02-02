9816fd15c7e57c3afc88f6247a7e62d2d62be709
DedKymus228 <sbaskatov5@gmail.com>
2026-02-02 19:58:16 +0200

    add API key validation and error handling

--- api/client.go
+++ api/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 type Movie struct {
@@ -15,6 +16,11 @@ type Movie struct {
 
 type MovieResponse struct {
 	Results []Movie `json:"results"`
+}
+
+type validApi struct {
+	Message string `json:"status_message"`
+	Success bool   `json:"success"`
 }
 
 func FetchMovies(apiKey string) ([]Movie, error) {
@@ -33,4 +39,48 @@ func FetchMovies(apiKey string) ([]Movie, error) {
 		return nil, err
 	}
 	return data.Results, nil
+}
+
+func CheckApiKey(fileNameWApi string) string {
+	var validApi validApi
+	validApi.Success = true
+	apiKey, err := os.ReadFile(fileNameWApi)
+	if err != nil {
+		fmt.Println("Error: TMDB API Key is missing!")
+		fmt.Println("To fix this, run: go run main.go -key \"YOUR_API_KEY\"")
+
+		return ""
+	}
+	url := "https://api.themoviedb.org/3/configuration?api_key=" + string(apiKey)
+	fmt.Println(url)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error", err)
+		return ""
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+
+	err = json.Unmarshal(body, &validApi)
+	if err != nil {
+		fmt.Println("Error", err)
+		return ""
+	}
+	if !validApi.Success {
+		fmt.Println(validApi.Message)
+		return ""
+	} else {
+		fmt.Println("API Key is valid")
+	}
+
+	return string(apiKey)
+
+}
+
+func SaveApiKey(fileNameWApi string, apiKey string) error {
+	err := os.WriteFile(fileNameWApi, []byte(apiKey), 0644)
+	if err != nil {
+		panic(err)
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -9,15 +9,25 @@ import (
 )
 
 func main() {
+	FileNameWApi := "api_key.txt"
 
+	keyFlag := flag.String("key", "", "TMDB API Key")
 	moviesType := flag.String("type", "popular", "Category of movies to fetch")
-	apiKey := os.Getenv("API_KEY")
 	endpoints := map[string]string{
 		"popular":  "popular",
 		"top":      "top_rated",
 		"upcoming": "upcoming",
 	}
 	flag.Parse()
+	var apiKey string
+
+	if *keyFlag != "" {
+		api.SaveApiKey(FileNameWApi, *keyFlag)
+		apiKey = api.CheckApiKey(FileNameWApi)
+		return
+	} else {
+		apiKey = api.CheckApiKey(FileNameWApi)
+	}
 	category, ok := endpoints[*moviesType]
 	if !ok {
 		fmt.Println("no category found")
@@ -30,8 +40,6 @@ func main() {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 0, ' ', tabwriter.Debug)
 	for place, movie := range movieOUT {
 		fmt.Fprintf(w, " #%d \t %s  \t Рейтинг: %.1f\n", place+1, movie.Title, movie.VoteAverage)
-		//todo tabulation
-
 	}
 	w.Flush()
 
